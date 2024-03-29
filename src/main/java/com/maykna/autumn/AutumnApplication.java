ef3b097aa5d1ff20e51521b8962c8d8c5e352609
khuongdt <khuongdt@hotmail.com>
2024-03-30 01:09:50 +0700

    Autumn Init

---
+++ src/main/java/com/maykna/autumn/AutumnApplication.java
@@ -0,0 +1,13 @@
+package com.maykna.autumn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutumnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutumnApplication.class, args);
+	}
+
+}
